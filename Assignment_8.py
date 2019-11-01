483ed28e8a2f8f527a0ee01840427d6e5de5cd56
SHALAKA JAVHERI <37468419+Shalakaj1234@users.noreply.github.com>
2019-11-01 16:08:18 +0530

    Add files via upload
    a program which accept one number and display below pattern.
    Input : 5
    Output : 1
    1 2
    1 2 3
    1 2 3 4
    1 2 3 4 5

--- Assignment_8.py
+++ Assignment_8.py
@@ -1,9 +1,12 @@
 def main():
+
 	print("Enter a number:");
 	no=int(input());
 
-	for i in range(no):
-		print("*",end='\t');
+	for i in range(0,no):
+		for j in range(0,i+1):
+			print(j+1,end="\t");
+		print("\n");
 
 if __name__=="__main__":
 	main();
