4c62d2b02efab0464b7bd2eb9a04b7bd7fc9279c
SHALAKA JAVHERI <37468419+Shalakaj1234@users.noreply.github.com>
2019-11-11 15:28:42 +0530

    Add files via upload
    Decimal to binary ,octal,hexadecimal using built in functions

---
+++ DecimalToBin_oct_hex.py
@@ -0,0 +1,11 @@
+def main():
+	no=int(input("Enter a decimal number: "));
+
+	print("Decimal no: ",no);
+	print("Binary no: ",bin(no));
+	print("Octal no: ",oct(no));
+	print("Hexadecimal no: ",hex(no));
+
+
+if __name__=="__main__":
+	main();
