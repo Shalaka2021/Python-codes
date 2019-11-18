d20d42c8232d95f305acfe4e96ccff82ecdb6ed9
SHALAKA JAVHERI <37468419+Shalakaj1234@users.noreply.github.com>
2019-11-18 16:59:39 +0530

    Add files via upload
    A program which contains one class named as Demo.
    Demo class contains two instance variables as no1 ,no2.
    That class contains one class variable as Value.
    There are two instance methods of class as Fun and Gun which displays values of instance
    variables.
    Initialise instance variable in init method by accepting the values from user

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,13 +1,34 @@
-def Pattern(no):
-	if(no!=0):
-		print("*",end=" ");
-		Pattern(no-1);
+class Demo:
+	Value=0;
 
+	def __init__(self,val1,val2):
+		self.no1=val1;
+		self.no2=val2;
+
+	def Fun(self):
+		print(self.no1,self.no2);
+	def Gun(self):
+		print(self.no1,self.no2);
 
 def main():
-	no=int(input("Enter a no: "));
+	no1=int(input("Enter no1: "));
+	no2=int(input("Enter no2: "));
 
-	Pattern(no);
+	obj1=Demo(no1,no2);
+	obj2=Demo(no1,no2);
+
+	obj1.Fun();
+	obj2.Fun();
+	obj1.Gun();
+	obj2.Gun();
+
+	obj3=Demo(11,21);
+	obj4=Demo(51,101);
+
+	obj3.Fun();
+	obj4.Fun();
+	obj3.Gun();
+	obj4.Gun();
 
 
 if __name__=="__main__":
